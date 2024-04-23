f560be714c62e8ac845c1b690d5ca41a1363be36
JoyNdimande <keotshepilendimande@gmail.com>
2024-04-23 14:49:52 +0200

    Add project files.

---
+++ myInterface/Mai.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myInterface
+{
+    internal class Mai
+    {
+        static void Main(string[] args)
+        {
+            Employees emp = new Employees("Joy Ndimande", 145236534, 10000.95, "Educator", "Llyod Jane");
+            Console.WriteLine("Employee Name: " + emp.GetEmployeeName());
+            Console.WriteLine("Employee ID: " + emp.GetEmployeeID());
+            Console.WriteLine("Salary: " + emp.GetSalary());
+            Console.WriteLine("Department: " + emp.GetDepartmentName());
+            Console.WriteLine("HOP: " + emp.GetHOP());
+            Console.WriteLine();
+
+
+            _ = new Employees("David Smith", 202033, 50000.95, "IT", "Lusukama Smith");
+            Console.WriteLine("Employee Name: " + emp.GetEmployeeName());
+            Console.WriteLine("Employee ID: " + emp.GetEmployeeID());
+            Console.WriteLine("Salary: " + emp.GetSalary());
+            Console.WriteLine("Department: " + emp.GetDepartmentName());
+            Console.WriteLine("HOP: " + emp.GetHOP());
+        }
+    }
+}
+
---
+++ myInterface/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+interface IEmployees
+{
+    string GetEmployeeName();
+    int GetEmployeeID();
+    double GetSalary();
+}
+
+interface IEDepartment
+{
+    string GetDepartmentName();
+    string GetHOP();
+}
+
+class Employees : IEmployees, IEDepartment   //Inheriting both
+{
+    private string employeeName;
+    private int employeeID;
+    private double salary;
+    private string departmentName;
+    private string HOP;
+    public Employees(string name, int id, double salary, string department, string hop)
+    {
+        this.employeeName = name;
+        this.employeeID = id;
+        this.salary = salary;
+        this.departmentName = department;
+        this.HOP = hop;
+    }
+    public string GetEmployeeName()
+    {
+        return employeeName;
+    }
+    public int GetEmployeeID()
+    {
+        return employeeID;
+    }
+    public double GetSalary()
+    {
+        return salary;
+    }
+    public string GetDepartmentName()
+    {
+        return departmentName;
+    }
+    public string GetHOP()
+    {
+        return HOP;
+    }
+
+    double IEmployees.GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+}
